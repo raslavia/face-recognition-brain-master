dbb789f4b13d59811dccdd579416586d0901354d
raslavia <raslavi.work@gmai.com>
2020-05-06 14:15:05 +0300

    heroku

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://murmuring-temple-93176.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
